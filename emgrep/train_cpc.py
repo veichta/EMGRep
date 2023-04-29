77612ff2bfaf4e4bcabeddb21d5e6ed166c57933
Alexander Veicht <veichta@ethz.ch>
2023-04-29 18:15:06 +0200

    Move criterion to device.

--- emgrep/train_cpc.py
+++ emgrep/train_cpc.py
@@ -138,6 +138,8 @@ def train_one_epoch_cpc(
     pbar = tqdm(dataloader, desc=f"Epoch {epoch+1} / {args.epochs_cpc}", ncols=100)
     losses = []
     model.to(args.device)
+    criterion.to(args.device)
+    model.train()
     for x, _, _ in pbar:
         optimizer.zero_grad()
         out = model(x.to(args.device))
@@ -178,6 +180,8 @@ def validate_cpc(
     pbar = tqdm(dataloader, desc=f"Val Epoch {epoch+1} / {args.epochs_cpc}", ncols=100)
     losses = []
     model.to(args.device)
+    criterion.to(args.device)
+    model.eval()
     with torch.no_grad():
         for x, y, _ in pbar:
             out = model(x.to(args.device))
@@ -215,6 +219,8 @@ def test(
     pbar = tqdm(dataloader, desc=f"Testing {epoch+1} / {args.epochs_cpc}", ncols=100)
     losses = []
     model.to(args.device)
+    criterion.to(args.device)
+    model.eval()
     with torch.no_grad():
         for x, y, _ in pbar:
             out = model(x.to(args.device))
