fd1c627e36ee64347baf10985948da202841e9bf
Tobi Scheithauer <b1t0@mailbox.org>
2023-04-29 16:54:43 +0200

    revert to old criterion version

--- emgrep/criterion.py
+++ emgrep/criterion.py
@@ -1,8 +1,7 @@
-"""Implementation of the InfoNCE loss function for CPC."""
+"""Implementation of the infoNCE loss function for CPC."""
 
 import torch
 import torch.nn as nn
-from einops import rearrange
 from info_nce import InfoNCE
 
 
@@ -15,7 +14,6 @@ class CPCCriterion(nn.Module):
         Args:
             k (int): Number of steps to look ahead for positive sampling. Must be > 0.
         """
-        # TODO: add weights W_k (do not need to be part of model, can be added here)
         super(CPCCriterion, self).__init__()
         assert k > 0, f"k must be > 0, but got {k}"
         self.k = k
@@ -24,13 +22,9 @@ class CPCCriterion(nn.Module):
     def forward(self, z: torch.Tensor, c: torch.Tensor) -> torch.Tensor:
         """Compute loss for CPC model.
 
-        ASSUME zSize = cSize <=> W_k === I for all k (same embedding dim, instead of z_{t+k}^TW_kc_t
-        like in paper. Conceptually assumes one W for all offsets
-        (implcit, GRU invariant down to lin. hom., right?)).
-
         Args:
-            z (torch.Tensor): Encoded batch, shape (batchSize, seqLen, zSize)
-            c (torch.Tensor): Autoregressive output, shape (batchSize, seqLen, cSize)
+            z (torch.Tensor): Encoded batch, shape (batchSize, zSize, seqLen)
+            c (torch.Tensor): Autoregressive output, shape (batchSize, cSize, seqLen)
 
         Returns:
             torch.Tensor: _description_
@@ -38,7 +32,6 @@ class CPCCriterion(nn.Module):
         Notes:
             - z and c must have the same shape, i.e., zSize == cSize is required.
         """
-        """legacy
         zBatchSize, zDim, zSeqLen = z.shape
         cBatchSize, cDim, cSeqLen = c.shape
         assert (
@@ -52,15 +45,6 @@ class CPCCriterion(nn.Module):
         zPos = z[:, :, self.k :]  # (batchSize, zSize, seqLen-k)
         zPos = zPos.permute(0, 2, 1)  # (batchSize, seqLen-k, zSize)
         zPos = zPos.reshape(-1, zDim)  # (batchSize * (seqLen-k), zSize)
-        """
-
-        # (... , cSize, seqLen-k, k-1)
-        cPos = torch.cat([c[..., :-k, :] for k in range(1, self.k + 1)], axis=-2)
-        cPos = rearrange(cPos, "B P2 L_k EmbC -> (B P2 L_k) EmbC")
-
-        # (..., zSize, seqLen-k, k-1)
-        zPos = torch.cat([z[..., k:, :] for k in range(1, self.k + 1)], axis=-2)
-        zPos = rearrange(zPos, "B P2 L_k EmbZ -> (B P2 L_k) EmbZ")
 
         # compute loss
         return self.infonce(cPos, zPos, zPos)
